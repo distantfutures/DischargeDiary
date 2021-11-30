999c87fa9631f85f5ab7dd024a304e1f70251046
Andrew-Zheng <andzheng@gmail.com>
2021-11-30 08:58:22 -0500

    Implemented ClickListener for dischargeType & added toast to check functionality

--- app/src/main/java/com/example/dischargediary/discharge/DischargeFragment.kt
+++ app/src/main/java/com/example/dischargediary/discharge/DischargeFragment.kt
@@ -7,11 +7,15 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import com.example.dischargediary.R
 import com.example.dischargediary.databinding.FragmentDischargeBinding
 
 class DischargeFragment : Fragment() {
+
+    private lateinit var viewModel: DischargeViewModel
+    //private lateinit var viewModelFactory: DischargeViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,21 +23,31 @@ class DischargeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding: FragmentDischargeBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_discharge, container, false)
+
+        //viewModelFactory = DischargeViewModelFactory(DischargeFragment.fromBundle(arguments!!).dischargeType)
+        viewModel = ViewModelProvider(this).get(DischargeViewModel::class.java)
+        binding.dischargeViewModel = viewModel
+        binding.lifecycleOwner = this
+
         binding.submitButton.setOnClickListener { view: View ->
             view.findNavController().navigate(R.id.action_discharge_fragment_to_discharge_diary_fragment)
         }
+
         binding.dischargeButtonToggleGroup.addOnButtonCheckedListener { _, checkedType, isChecked ->
             if (isChecked) {
-                //Change value of dischargeType to 1 or 2
+                //Changes value of dischargeType to 1 or 2
                 when (checkedType) {
-                    R.id.numberOneButton -> showToast("One")//set LiveData to 1
-                    R.id.numberTwoButton -> showToast("Two")//_dischargeType.value = 2//set LiveData to 2
+                    R.id.numberOneButton -> viewModel.onSetDischargeType(1)//set LiveData to 1
+                    R.id.numberTwoButton -> viewModel.onSetDischargeType(2) //_dischargeType.value = 2//set LiveData to 2
                 }
+                //checks value type
+                val toast = viewModel.dischargeType.value.toString()
+                showToast(toast)
             }
         }
         return binding.root
     }
-    private fun showToast(str: String) {
-        Toast.makeText(context, str, Toast.LENGTH_LONG).show()
+    fun showToast(str: String) {
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/dischargediary/discharge/DischargeViewModel.kt
+++ app/src/main/java/com/example/dischargediary/discharge/DischargeViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DischargeViewModel() : ViewModel() {
+//number: Int
     private val _dischargeType = MutableLiveData<Int>()
     val dischargeType: LiveData<Int>
         get() = _dischargeType
@@ -12,4 +13,13 @@ class DischargeViewModel() : ViewModel() {
     init {
         _dischargeType.value = 0
     }
+
+    fun onSetDischargeType(dischargeType: Int) {
+        _dischargeType.value = dischargeType
+        //showToast(dischargeType.toString())
+    }
+
+//    fun showToast(str: String) {
+//        Toast.makeText(this, str, Toast.LENGTH_LONG).show()
+//    }
 }
