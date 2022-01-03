57f0b459b5ec37d4b8701f484c34ce7b876f5a22
Andrew-Zheng <andzheng@gmail.com>
2022-01-03 11:55:38 -0500

    Util format update for Diary Fragment

--- app/src/main/java/com/example/dischargediary/Util.kt
+++ app/src/main/java/com/example/dischargediary/Util.kt
@@ -13,15 +13,11 @@ fun formatDischarges(discharges: List<DischargeData>, resources: Resources): Spa
         discharges.forEach {
             append("<br><b>Date: </b>${it.dischargeDate}")
             append("<br><b>Time: </b>${it.dischargeTime}")
-            append("<br><b>Type: </b>${it.dischargeType}")
+            append("<br><b>Type: </b> #${it.dischargeType}")
             append("<br><b>Duration: </b>${it.dischargeDuration}")
             append("<br><b>Leakage: </b>${it.leakage}")
             append("<br><b>Color: </b>${it.dischargeColor}")
-            if (it.dischargeConsistency == null) {
-                append("<br><b>Consistency: </b>N/A")
-            } else {
-                append("<br><b>Consistency: </b>${it.dischargeConsistency}")
-            }
+            append("<br><b>Consistency: </b>${it.dischargeConsistency}")
             append("<br>")
         }
     }
