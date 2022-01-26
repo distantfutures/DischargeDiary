26c2d6515438f07ed53adbe48926a67cf14c31ef
Andrew Zheng <andzheng@gmail.com>
2022-01-26 11:51:52 -0500

    Revert "Updated dependencies and added module & application class"
    This reverts commit e5a8b8fc66c053fbf07122c575263ef16ef4983f.

--- app/src/main/java/com/example/dischargediary/AppModule.kt
+++ app/src/main/java/com/example/dischargediary/AppModule.kt
@@ -1,18 +1,4 @@
 package com.example.dischargediary
 
-import com.example.dischargediary.data.DischargeDatabaseDao
-import dagger.Module
-import dagger.Provides
-import dagger.hilt.InstallIn
-import javax.inject.Singleton
-
-// Create an Application Module
-// Lives as long as the application
-@Module
-@InstallIn(Singleton::class)
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun dataInject(database: DischargeDatabaseDao) = database
 }
--- app/src/main/java/com/example/dischargediary/DDApplication.kt
+++ app/src/main/java/com/example/dischargediary/DDApplication.kt
@@ -1,9 +1,4 @@
 package com.example.dischargediary
 
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-// Create Application Class
-// Hilt uses this to create classes behind the scenes
-@HiltAndroidApp
-class DDApplication : Application()
+class DDApplication {
+}
