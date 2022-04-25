69373494608f6b8fa07b20edfcb56ee488c6254d
Andrew Zheng <andzheng@gmail.com>
2022-04-25 12:22:39 -0400

    Updated Reminder Notifications to be fire daily with 2 hour intervals

--- app/src/main/java/com/example/dischargediary/MainActivity.kt
+++ app/src/main/java/com/example/dischargediary/MainActivity.kt
@@ -8,9 +8,9 @@ import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
@@ -24,8 +24,8 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.M)
     fun scheduleNotification() {
         val intent = Intent(applicationContext, Notification::class.java)
-        val title = "Notification"
-        val message = "Discharge reminder engaged"
+        val title = "Discharge Diary Reminder!"
+        val message = "Have you gone today? Log it!"
         intent.putExtra(titleExtra, title)
         intent.putExtra(messageExtra, message)
 
@@ -37,10 +37,14 @@ class MainActivity : AppCompatActivity() {
         )
 
         val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-        val time = Calendar.getInstance().timeInMillis + 1000
-        alarmManager.setExactAndAllowWhileIdle(
-            AlarmManager.RTC_WAKEUP,
-            time,
+        val triggerTime = SystemClock.elapsedRealtime() + AlarmManager.INTERVAL_HALF_DAY
+        val intervalTime = AlarmManager.INTERVAL_HOUR + AlarmManager.INTERVAL_HOUR
+        alarmManager.setInexactRepeating(
+            // Elapsed Realtime Wakeup is more scalable due to locale changes
+            // or system time changes. vs RTC Wakeup
+            AlarmManager.ELAPSED_REALTIME_WAKEUP,
+            triggerTime,
+            intervalTime,
             pendingIntent
         )
     }
@@ -48,9 +52,10 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.O)
     fun createNotificationChannel() {
         val name = "Notification Channel"
-        val description = "A Description of the Channel"
+        val description = "A Description of the Channel in the Settings"
         val importance = NotificationManager.IMPORTANCE_DEFAULT
         val channel = NotificationChannel(channelId, name, importance)
+        channel.description = description
         val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
     }
