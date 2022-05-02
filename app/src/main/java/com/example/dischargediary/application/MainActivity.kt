59f01f06ef3b5cff3349c33c25b06b79e991d094
Andrew Zheng <andzheng@gmail.com>
2022-05-02 10:55:34 -0400

    Created login Layout

--- app/src/main/java/com/example/dischargediary/application/MainActivity.kt
+++ app/src/main/java/com/example/dischargediary/application/MainActivity.kt
@@ -5,12 +5,20 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dischargediary.R
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    /**
+     * [Part1] Get ref to Firebase Authentication Object
+     **/
+    lateinit var auth: FirebaseAuth
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // [Part2] Get instance of auth
+        auth = FirebaseAuth.getInstance()
 
         scheduleNotification(this)
     }
